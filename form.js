ce694d2d599a92ed5c69bb549e15c57f85de2d45
Zülkarneyn Yavaş <zulkarneynyavas@gmail.com>
2020-05-11 12:39:47 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,14 +1,14 @@
 function FormSubmit(form) {
-  var xhr = new XMLHttpRequest(),
-    serialize = new FormData(form),
-    button = form.getElementsByTagName("button");
-  xhr.addEventListener("load", function(event){
-    button[0].innerHTML = event.target.responseText;
-  });
-  xhr.addEventListener("error", function(event){
-    button[0].innerHTML = 'error';
-  });
-  xhr.open('post', 'post.php');
-  xhr.send(serialize);
-  return false;
+	var xhr = new XMLHttpRequest(),
+		serialize = new FormData(form),
+		button = form.getElementsByTagName("button");
+	xhr.addEventListener("load", function(event){
+		button[0].innerHTML = event.target.responseText;
+	});
+	xhr.addEventListener("error", function(event){
+		button[0].innerHTML = 'error';
+	});
+	xhr.open('post', 'post.php');
+	xhr.send(serialize);
+	return false;
 }
